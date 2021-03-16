3230891bdc0750f936ec4a1737a538ad0c24fbde
peichan517 <72973248+peichan517@users.noreply.github.com>
2021-03-16 12:05:43 +0900

    Add files via upload

---
+++ post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
+
+  belongs_to :user
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
+end
